0de4caba87bf30cd03247d56bbf312a187004cd4
Alan-N-Pereira <itsalanpereira@gmail.com>
2022-04-07 00:28:38 +0530

    Add note add component

---
+++ src/component/addNote/AddNote.jsx
@@ -0,0 +1,12 @@
+import './addNote.css'
+
+export default function AddNote() {
+  return (
+    <div className='addNote-container'>
+        <div className="add">
+            <i className="add-icon fa-solid fa-circle-plus"></i>
+        </div>
+        <div className="addNote-title">Add Note</div>
+    </div>
+  )
+}
--- src/component/notesList/NoteList.jsx
+++ src/component/notesList/NoteList.jsx
@@ -1,10 +1,12 @@
 
+import AddNote from '../addNote/AddNote'
 import Note from '../note/Note'
 import './noteList.css'
 
 export default function NoteList({notes}) {
   return (
     <div className='noteList-container'>
+      <AddNote/>
       {notes.map((note)=><Note key={note.id} note={note}/>)}
     </div>
   )
